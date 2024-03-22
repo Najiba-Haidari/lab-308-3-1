8da2d8bd049814d1e657c83a14bbfa15151ed003
Najiba-Haidari <nhaidari089@gmail.com>
2024-03-21 21:34:18 -0600

    final

--- script.js
+++ script.js
@@ -1,87 +1,126 @@
 // PART 1
+console.log("\n\n");
 console.log("PART 1");
-for (let i = 1; i < 101; i++) {
-    // console.log(i);
-    if (i % 3 === 0) {
-        console.log(i, "Fizz");
-        if (i % 5 === 0) {
-            console.log(i, "Fizz Buzz")
-        }
-    } else if (i % 5 === 0) {
-        console.log(i, "Buzz");
-    } else {
-        console.log(i);
-    }
+
+var str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+// console.log(str);
+var cell = "";
+var row = "";
+
+for (i = 0; i < str.length; i++) {
+  if (str[i] !== ",") {
+    cell += str[i];
+    console.log();
+  }
+  if (str[i] === ",") {
+    row += cell;
+    // console.log(row)
+    cell = " ";
+  }
+  if (str[i] === "\n") {
+    row += cell;
+    console.log(row);
+    cell = " ";
+    row = " ";
+  }
 }
-// for (let i = 1; i <= 100; i++){
-//     if (i % 3 === 0 && i % 5 === 0){
-//         console.log ('Fizz Buzz')
-//     }else if (i % 3 === 0){
-//         console.log('Fizz ')
-//     }else if (i % 5 === 0){
-//         console.log('Buzz')
-//     }else{
-//         console.log(i);
+
+// for (const x in str){
+//     if(str[x] == "\n"){
+//         continue;
 //     }
 // }
+// console.log(str);
 
-//PART 2
+// PART 2
+// Store your results in a two-dimensional array .
+// Each row should be its own array, with individual entries for each column.
+// Cache this two-dimensional array in a variable for later use.
 console.log("\n\n");
 console.log("PART 2");
-for (let n = 2; n < 30; n++) {
-    var isPrime = true;
-    for (let m = 2; m < n; m++) {
-        //to skip if the numbers are not prime
-        if (n % m === 0){
-            isPrime = false;
-            // console.log(m,'not a prime')
-            continue;
-        }
-    }
-    if (isPrime){
-        console.log(n, 'is a prime number')
-    }
+var arrays = str.split("\n");
+console.log(arrays);
+var finalArr = [];
+for (let array of arrays) {
+  var singleArr = array.split(",");
+  // console.log(singleArr);
+  finalArr.push(singleArr);
 }
+console.log(finalArr);
 
 // PART 3
 console.log("\n\n");
 console.log("PART 3");
+var arrayOfObjects = [];
 
-var str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
-
-for (const x in str){
-    if(str[x] == "\n"){
-        continue;
-    }
-    console.log(str);
+for (let i = 1; i < finalArr.length; i++) {
+  var object = {};
+  for (let j = 0; j < finalArr[0].length; j++) {
+    // object.id = finalArr[i][j]
+    object[finalArr[0][j]] = finalArr[i][j];
+    // console.log("hello",finalArr[0][j]);
+  }
+  arrayOfObjects.push(object);
 }
+// console.log(arrayOfObjects);
+// arrayOfObjects.concat(object[0],object[1],object[2],object[3],object[4])
+console.log(arrayOfObjects);
+console.log(arrayOfObjects.length);
 
-// Your task is to write a script that accomplishes the following:
-// Loop through the characters of a given CSV string.
+// PART 4
+//     1. Remove the last element from the sorted array .
+// 2. Insert the following object at index 1:
+// { id: "48", name: "Barry", occupation: "Runner", age: "25" }
+// 3. Add the following object to the end of the array:
+// { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+console.log("\n\n");
+console.log("PART 4");
+var newArrayOfObject = arrayOfObjects.splice(0, 4);
+newArrayOfObject[1] = {
+  ID: "48",
+  Name: "Barry",
+  Occupation: "Runner",
+  Age: "25",
+};
+newArrayOfObject.push({
+  ID: "7",
+  Name: "Bilbo",
+  Occupation: "None",
+  Age: "111",
+});
+console.log(newArrayOfObject);
+// console.log('line 84')
 
-str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`
-let cell = [];
-let row= [];
-let cellIndicator = 0;
-for (const i in str){
-// Store each "cell" of data in a variable.
-
-// When you encounter a comma, move to the next cell.
-    if (str[i] === ','){
-        cellIndicator++;
-        continue;
-    }
-// When you encounter the "\n" sequence, move to the next "row."
-    if (str[i] === '\n'){
-        console.log(cell[0],cell[1],cell[2],cell[3]);
-        cellIndicator=0;
-        cell = [];
-        continue;
-    }
-    if (cell[cellIndicator]){
-    cell[cellIndicator] = cell[cellIndicator] + str[i];
-    }else{
-        cell[cellIndicator] = str[i];
-    }
-    
+//use the values of each object within the array and the array’s length property to calculate the
+// average age of the group. This calculation should be accomplished using a loop.
+console.log("average age of of the group");
+// console.log(newArrayOfObject[i]);
+let sum = 0;
+for (let i = 0; i < newArrayOfObject.length; i++) {
+  // console.log(newArrayOfObject[i].Age);
+  sum += Number(newArrayOfObject[i].Age);
+  // console.log(sum);
 }
+// console.log(sum);
+let average = sum / newArrayOfObject.length;
+console.log(average);
+
+//PART 5
+// As a final task, transform the final set of data back into CSV format.
+console.log("\n\n");
+console.log("PART 5");
+
+// console.log(newArrayOfObject);
+let csv = "";
+for (let i = 0; i < newArrayOfObject.length; i++) {
+  csv +=
+    newArrayOfObject[i].ID +
+    ", " +
+    newArrayOfObject[i].Name +
+    ", " +
+    newArrayOfObject[i].Occupation +
+    ", " +
+    newArrayOfObject[i].Age +
+    ", ";
+}
+console.log(csv);
